4d383178bfeca6df6db928c0a126ae6c95652e4e
Gyesik Lee <gslee@hknu.ac.kr>
2022-10-17 15:59:07 +0900

    ch06 is ready

--- jupyter-book/_build/jupyter_execute/python_list_dict.py
+++ jupyter-book/_build/jupyter_execute/python_list_dict.py
@@ -3,9 +3,15 @@
 
 # # 파이썬 모음 자료형 분석
 
+# **소스코드**
+# 
+# 아래 내용을 
+# [(구글 코랩) 파이썬 모음 자료형 분석](https://colab.research.google.com/github/codingalzi/algopy/blob/master/jupyter-book/python_list_dict.ipynb)에서 
+# 직접 실행할 수 있다.
+
 # **주요 내용**
 # 
-# - 파이썬 리스트와 사전 자료형 메서드의 시간복잡도
+# - 파이썬 리스트 자료형과 사전 자료형 메서드의 시간복잡도 비교
 # - 자료형 클래스 정의 방식에 따른 알고리즘 복잡도 변화
 
 # 파이썬에서 기본으로 제공하는 두 개의 컨테이너 자료형인 리스트(`list`)와 사전(`dict`)
@@ -13,6 +19,7 @@
 # 알고리즘의 시간복잡도를 Big-O를 이용하여 분석한다.
 # 리스트와 사전은 계속해서 다른 자료구조를 구현할 때 기본적으로 사용되는 요소들이기에
 # 관련된 알고리즘들의 성능을 이해하는 일이 매우 중요하다. 
+# 
 # 여기서는 성능의 차이가 왜 발생하는가를 설명하진 않으며 단지 어떤 성능 차이가 있는지만을 확인한다. 
 # 하지만 나중에 리스트와 사전을 다른 방식으로 직접 구현해 보면서 구현방법에 따른 알고리즘 
 # 성능의 차이를 살펴볼 것이다. 
@@ -20,20 +27,20 @@
 # ## 리스트
 
 # 파이썬과 관련된 많은 메서드(함수)들의 성능이 종류에 따라 다르다.
-# 기본적으로 가장 많이 사용되는 메서드일 수록 빠르며, 그렇지 않은 경우는 보다 느리다.
+# 기본적으로 가장 많이 사용되는 메서드일수록 빠르다.
 
 # ### 인덱싱
 
-# 예를 들어, 인덱싱과 인덱스를 사용항 항목 지정이 리스트와 관련되어 가장 많이 사용되는데
+# 예를 들어, 인덱싱과 인덱싱을 활용한 항목 지정이 리스트와 관련되어 가장 많이 사용되는데
 # 이 두 기능을 수행하는 알고리즘은 상수의 시간복잡도인 $O(1)$을 갖는다.
 
-# ### 리스트 확장
+# ### 리스트 확장 메서드
 
 # 리스트를 확장하기 위해 최소 세 가지 방식을 사용할 수 있다. 
 # 
 # - `append()` 메서드
 # - `extend()` 메서드
-# - 리스트 연결(concatenation) 연산자 `+`
+# - 리스트 연결(concatenation) 연산자(`+`)
 
 # In[1]:
 
@@ -64,13 +71,12 @@ aList
 # 
 # - `append()`: $O(1)$
 # - `extend()`: $O(1)$
-# - 리스트 연결: $O(k)$
-#     - $k$는 `__add__()`를 호출하는 리스트의 길이.
-#     즉, `aList + [4, 5]`에서 `aList`.
+# - 리스트 연결: $O(k)$: $k$는 `__add__()`를 호출하는 리스트의 길이.
+#     즉, `aList + [4, 5]`에서 `aList`의 길이.
 # 
 # 아래 코드는 길이가 $n$인 리스트를 생성하는 5 가지 알고리즘의 실행시간을 계산한다. 
 # 앞서 언급한 세 가지 연산자 이외에 비교를 위해 리스트 조건제시법과
-# `range` 객체를 이용하는 방법을 추가한다.
+# `range` 객체를 이용하는 방법도 함께 살펴본다.
 
 # **`timeit` 모듈 활용**
 
@@ -131,9 +137,9 @@ def test5():
 from timeit import Timer
 
 t1 = Timer("test1()", "from __main__ import test1")
-print(f"append() 메서드:\t\t {t1.timeit(number=1000):.2f} 초")
+print(f"append() 메서드:\t {t1.timeit(number=1000):.2f} 초")
 t1_ = Timer("test2()", "from __main__ import test2")
-print(f"extend() 메서드:\t\t {t1_.timeit(number=1000):.2f} 초")
+print(f"extend() 메서드:\t {t1_.timeit(number=1000):.2f} 초")
 t2 = Timer("test3()", "from __main__ import test3")
 print(f"리스트 연결 연산자:\t {t2.timeit(number=1000):.2f} 초")
 t3 = Timer("test4()", "from __main__ import test4")
@@ -148,8 +154,6 @@ print(f"range 객체:\t\t {t4.timeit(number=1000):.2f} 초")
 #     기본적으로 실행시간이 세 배 더 걸린다.
 # - 하지만 리스트 연결 연산자(`+`)의 경우 아홉 배 걸린다.
 #     이유는 반복문의 횟수 뿐만 아니라 연산자의 실행시간 자체도 세 배 더 걸리기 때문이다.
-# 
-# - `range` 객체를 사용할 때 실행시간이 거의 변하지 않는다.
 
 # In[5]:
 
@@ -185,9 +189,9 @@ def test5():
 from timeit import Timer
 
 t1 = Timer("test1()", "from __main__ import test1")
-print(f"append() 메서드:\t\t {t1.timeit(number=1000):.2f} 초")
+print(f"append() 메서드:\t {t1.timeit(number=1000):.2f} 초")
 t1_ = Timer("test2()", "from __main__ import test2")
-print(f"extend() 메서드:\t\t {t1_.timeit(number=1000):.2f} 초")
+print(f"extend() 메서드:\t {t1_.timeit(number=1000):.2f} 초")
 t2 = Timer("test3()", "from __main__ import test3")
 print(f"리스트 연결 연산자:\t {t2.timeit(number=1000):.2f} 초")
 t3 = Timer("test4()", "from __main__ import test4")
@@ -350,7 +354,9 @@ for i in range(10_000, 1_010_001, 100_000):
 # <div align="center"><img src="https://runestone.academy/runestone/books/published/pythonds3/_images/listvdict.png" width="70%"></div>
 # </figure>
 
-# **참고**: 리스트, 사전, 집합, 덱(deque)의 시간복잡도에 대한 최신 정보는 
+# ## 기타 모음 자료형 시간복잡도
+
+# 리스트, 사전, 집합, 덱(deque) 등 파이썬 모음 자료형의 시간복잡도에 대한 최신 정보는 
 # [Time Complexity Wiki](http://wiki.python.org/moin/TimeComplexity)에서
 # 확인할 수 있다.
 
